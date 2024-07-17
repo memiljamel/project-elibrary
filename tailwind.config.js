b06542e37a9a9c50151f7c16f4d8bf5bf00e0c70
Emil Jamel Mahmuda <memiljamel@gmail.com>
2024-07-17 16:57:06 +0700

    chore: install utilities css with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './wwwroot/*.html',
+    './Views/**/*.cshtml',
+    './Pages/**/*.cshtml',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
